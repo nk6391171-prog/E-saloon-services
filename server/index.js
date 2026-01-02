46ec5e68d3e2f0a6a91f87db241d6c997ba9cb09
nk6391171-prog <nk6391171@gmail.com>
2026-01-02 15:12:08 +0530

    yty

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@
         const app=express();
        
         app.use(cors({
-            origin:"https://esaloonservices.netlify.app"
+            origin:"https://esaloonservices.netlify.app/"
         }));
 
 
