805f7e984a316192999de329df3c0fd201b51874
nk6391171-prog <nk6391171@gmail.com>
2026-01-03 07:36:20 +0530

    abc

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@
         const app=express();
        
         app.use(cors({
-            origin:"https://esaloonservices.netlify.app/"
+            origin:"esaloonservicesbholenath.netlify.app"
         }));
 
 
